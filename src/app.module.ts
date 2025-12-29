2aaa4cfceec354aed006534ce7819a93ac13bb48
baslani <behzadaslani80@gmail.com>
2025-12-29 16:03:59 +0330

    connected to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,28 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        username: 'postgres',
+        entities: [],
+        synchronize: true, // use only in development
+        port: 5432,
+        password: 'behzad@postgres',
+        host: 'localhost',
+        database: 'nest-blog',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
