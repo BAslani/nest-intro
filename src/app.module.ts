58909005c997983b73ab623f745c3b3fbb8f74ed
baslani <behzadaslani80@gmail.com>
2026-01-28 10:52:02 +0330

    implemented database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,36 +4,33 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
 import { PostsModule } from './posts/posts.module';
 import { TagsModule } from './tags/tags.module';
 import { UsersModule } from './users/users.module';
 
 const ENV = process.env.NODE_ENV?.trim();
-
-console.log('ENV CHECK:', {
-  NODE_ENV: process.env.NODE_ENV,
-  DB_PASS: process.env.DATABASE_PASSWORD,
-  TYPE: typeof process.env.DATABASE_PASSWORD,
-});
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      load: [appConfig, databaseConfig],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        username: configService.get('DATABASE_USER'),
-        autoLoadEntities: true,
-        synchronize: true, // use only in development
-        port: +configService.get('DATABASE_PORT'),
-        password: configService.get('DATABASE_PASSWORD'),
-        host: configService.get('DATABASE_HOST'),
-        database: configService.get('DATABASE_NAME'),
+        username: configService.get('database.user'),
+        autoLoadEntities: configService.get('database.autoloadEntities'),
+        synchronize: configService.get('database.synchronize'), // use only in development
+        port: +configService.get('database.port'),
+        password: configService.get('database.password'),
+        host: configService.get('database.host'),
+        database: configService.get('database.name'),
       }),
     }),
     UsersModule,
